075403c36af56a9257c8c0a8ec14c66a909d82e8
argorn-inc <70586243+argorn-inc@users.noreply.github.com>
2020-10-08 23:44:27 +0000

    Component1

---
+++ components/src/components/Component1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FirstComponents(){
+    return(
+        <div>
+            <h1>Welcome to React Programming</h1>
+            <p>This is my Component Assignment. Enjoy!</p>
+        </div>
+    );
+}
+export default FirstComponents;
