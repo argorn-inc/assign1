115469143c761bb838f5db4425e4c35a0f8fc7a2
argorn-inc <70586243+argorn-inc@users.noreply.github.com>
2020-10-09 00:06:04 +0000

    Component2

--- components/src/App.js
+++ components/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import First from './components/Component1';
+import Second from './components/Component2';
 
 function App() {
   return (
     <div className="App">
       <First/>
-      
+      <Second/>
     </div>
   );
 }
---
+++ components/src/components/Component2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyPic from '../img/mypic.jpg';
+
+function SecondComponent(){
+    return(
+        <p>Attempting to import a picture</p>
+        
+    );
+}
+
+export default SecondComponent;
