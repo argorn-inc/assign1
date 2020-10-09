e1d9ecb839bbec1e15fb1e82186f12394523b2ce
argorn-inc <70586243+argorn-inc@users.noreply.github.com>
2020-10-09 21:47:45 +0000

    component6

--- components/src/App.js
+++ components/src/App.js
@@ -6,11 +6,13 @@ import Second from './components/Component2';
 import Third from './components/Component3';
 import Fourth from './components/Component4';
 import Fifth from './components/Component5';
+import Sixth from './components/Component6';
 
 
 function App() {
   return (
     <div className="App">
+      <Sixth/>
       <First />
       <Second/>
       <Third/>
---
+++ components/src/components/Component6.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './components.css';
+
+function SixthComponent(){
+    return (
+        <div className="sixth">
+            <h2> Building a navbar. Component6</h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </div>
+    );
+}
+export default SixthComponent;
